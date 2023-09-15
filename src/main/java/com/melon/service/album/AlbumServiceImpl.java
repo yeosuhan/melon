6c3adba2e21b6e7d5afd14558f47218789a3c9df
dlagnlwo <105468233+dlagnlwo@users.noreply.github.com>
2023-09-15 14:32:18 +0900

    Add files via upload

--- src/main/java/com/melon/service/album/AlbumServiceImpl.java
+++ src/main/java/com/melon/service/album/AlbumServiceImpl.java
@@ -2,7 +2,9 @@ package com.melon.service.album;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import com.melon.dto.album.AlbumDetails;
+import com.melon.dto.comment.CommentDto;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import com.melon.dao.album.IAlbumDao;
@@ -10,15 +12,118 @@ import com.melon.dto.album.AlbumDto;
 
 import lombok.extern.log4j.Log4j2;
 @Service
+@RequiredArgsConstructor
 @Log4j2
 public class AlbumServiceImpl implements IAlbumService {
 
 	private final IAlbumDao albumDao;
-	
-	@Autowired
-	public AlbumServiceImpl(IAlbumDao albumDao) {
-		this.albumDao = albumDao;
+
+	/**
+	 * 앨범 정보
+	 *
+	 * @author 임휘재
+	 */
+	@Override
+	public AlbumDetails getAlbumDetails(int albumId) {
+		return albumDao.selectAlbumDetails(albumId);
 	}
+
+	/**
+	 * 앨범 수록곡 조회
+	 *
+	 * @author 임휘재
+	 */
+	@Override
+	public List<AlbumDetails> getAlbumSongList(int albumId) {
+		return albumDao.selectAlbumSongList(albumId);
+	}
+
+	/**
+	 * 앨범 상세 조회
+	 *
+	 * @author 임휘재
+	 */
+	@Override
+	public AlbumDetails getAlbumDetail(int albumId) {
+		return albumDao.selectAlbumDetail(albumId);
+	}
+
+	/**
+	 * 앨범 댓글 작성
+	 *
+	 * @author 임휘재
+	 */
+	@Override
+	public void saveComment(CommentDto commentDto, int albumId) {
+		albumDao.saveComment(albumId, commentDto);
+	}
+
+	/**
+	 * 앨범 댓글 조회
+	 *
+	 * @author 임휘재
+	 */
+	@Override
+	public List<CommentDto> getAlbumCommentList(int albumId) {
+		List<CommentDto> dto =  albumDao.selectMyComment(albumId);
+		dto.sort((c1, c2) -> c2.getCommentDate().compareTo(c1.getCommentDate()));
+		return dto;
+	}
+
+	/**
+	 * 앨범 댓글 삭제
+	 *
+	 * @author 임휘재
+	 */
+	@Override
+	public void deleteComment(int commentId, int albumId) {
+		albumDao.deleteComment(albumId, commentId);
+	}
+
+	/**
+	 * 앨범 좋아요 증가
+	 *
+	 * @author 임휘재
+	 */
+	@Override
+	public void albumLikeUpdate(int albumId) {
+		albumDao.albumLikeUpdate(albumId);
+	}
+
+	/**
+	 * 앨범 좋아요 삭제
+	 *
+	 * @author 임휘재
+	 */
+	@Override
+	public void albumLikeDelete(int albumId) {
+		albumDao.albumLikeDelete(albumId);
+	}
+
+	/**
+	 * 앨범 좋아요 조회
+	 *
+	 * @author 임휘재
+	 */
+	@Override
+	public AlbumDetails selectAlbumLike(int albumId) {
+		return albumDao.selectAlbumLike(albumId);
+	}
+
+	/**
+	 * 앨범의 좋아요를 좋아요 테이블에 저장
+	 *
+	 * @author 임휘재
+	 */
+	@Override
+	public void albumLikeToUserLike(int albumId, String memberId) {
+		try {
+			albumDao.AlbumLikeToUserLike(albumId, memberId);
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+
 	/**
 	* 최신앨범 조회
 	* @author 여수한
--- src/main/java/com/melon/service/album/IAlbumService.java
+++ src/main/java/com/melon/service/album/IAlbumService.java
@@ -2,9 +2,81 @@ package com.melon.service.album;
 
 import java.util.List;
 
+import com.melon.dto.album.AlbumDetails;
 import com.melon.dto.album.AlbumDto;
+import com.melon.dto.comment.CommentDto;
 
 public interface IAlbumService {
+
+	/**
+	 * 앨범 기본 정보 조회
+	 *
+	 * @author 임휘재
+	 */
+	AlbumDetails getAlbumDetails(int albumId);
+
+	/**
+	 * 앨범의 수록곡 전체 조회
+	 *
+	 * @author 임휘재
+	 */
+	List<AlbumDetails> getAlbumSongList(int albumId);
+
+	/**
+	 * 앨범 상세 조회
+	 *
+	 * @author 임휘재
+	 */
+	AlbumDetails getAlbumDetail(int albumId);
+
+	/**
+	 * 앨범 댓글 작성
+	 *
+	 * @author 임휘재
+	 */
+	void saveComment(CommentDto commentDto, int albumId);
+
+	/**
+	 * 앨범 댓글 조회
+	 *
+	 * @author 임휘재
+	 */
+	List<CommentDto> getAlbumCommentList(int albumId);
+
+	/**
+	 * 앨범 댓글 삭제
+	 *
+	 * @author 임휘재
+	 */
+	void deleteComment(int commentId, int albumId);
+
+	/**
+	 * 앨범 좋아요 증가
+	 *
+	 * @author 임휘재
+	 */
+	void albumLikeUpdate(int albumId);
+
+	/**
+	 * 앨범 좋아요 삭제
+	 *
+	 * @author 임휘재
+	 */
+	void albumLikeDelete(int albumId);
+
+	/**
+	 * 앨범 좋아요 조회
+	 *
+	 * @author 임휘재
+	 */
+	AlbumDetails selectAlbumLike(int albumId);
+
+	/**
+	 * 앨범의 좋아요를 좋아요 테이블에 저장
+	 *
+	 * @author 임휘재
+	 */
+	void albumLikeToUserLike(int albumId, String memberId);
 
 	/**
 	* 최신앨범 조회
