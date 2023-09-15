baca2ebf875146f68f4d3a40f2c6cd0e1e2cbb12
positivejh <124271627+positivejh@users.noreply.github.com>
2023-09-15 15:05:49 +0900

    관리자 dto

---
+++ src/main/java/com/melon/dto/admin/AdminArtistDto.java
@@ -0,0 +1,14 @@
+package com.melon.dto.admin;
+
+import lombok.Data;
+
+@Data
+public class AdminArtistDto {
+
+    private int artistId;
+    private String artistName;
+    private String debut;
+    private String agency;
+    private String activityType;
+    private String artistPro;
+}
---
+++ src/main/java/com/melon/dto/admin/AdminMemberDto.java
@@ -0,0 +1,14 @@
+package com.melon.dto.admin;
+
+import lombok.Data;
+
+@Data
+public class AdminMemberDto {
+
+    private String id;
+    private String name;
+    private String nick;
+    private String birthday;
+    private String tel;
+    private String email;
+}
