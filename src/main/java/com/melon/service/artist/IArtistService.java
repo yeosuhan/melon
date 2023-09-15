6654d65bce786d28b9371803dfdc49253e80319b
dlagnlwo <105468233+dlagnlwo@users.noreply.github.com>
2023-09-15 14:41:07 +0900

    Add files via upload

--- src/main/java/com/melon/service/artist/IArtistService.java
+++ src/main/java/com/melon/service/artist/IArtistService.java
@@ -3,7 +3,6 @@ package com.melon.service.artist;
 import com.melon.dto.artist.ArtistDto;
 
 import java.util.List;
-import java.util.Map;
 
 public interface IArtistService {
 
@@ -34,6 +33,78 @@ public interface IArtistService {
      * @author 임휘재
      */
     ArtistDto getArtistActivity(int artistId);
+
+    /**
+     * 가수 노래 최신순, 인기순, 가나다순
+     *
+     * @author 임휘재
+     */
+    List<ArtistDto> getArtistSongsSort(int artistId, String sortType);
+
+    /**
+     * 가수 앨범 목록 중복 제거
+     *
+     * @author 임휘재
+     */
+    List<ArtistDto> ArtistAlbumsRemoveDuplication(int artistId);
+
+    /**
+     * 가수 좋아요 증가
+     *
+     * @author 임휘재
+     */
+    void ArtistLikeUpdate(int artistId);
+
+    /**
+     * 가수 좋아요 삭제
+     *
+     * @author 임휘재
+     */
+    void ArtistLikeDelete(int artistId);
+
+
+    /**
+     * 현재 가수 좋아요 수
+     *
+     * @author 임휘재
+     */
+    ArtistDto getArtistLikeCntNow(int artistId);
+
+    /**
+     * 가수 곡 좋아요 증가
+     *
+     * @author 임휘재
+     */
+    void ArtistSongLikeUpdate(int artistId);
+
+
+    /**
+     * 가수 곡 좋아요 삭제
+     *
+     * @author 임휘재
+     */
+    void ArtistSongLikeDelete(int artistId);
+
+    /**
+     * 가수 곡 좋아요 수
+     *
+     * @author 임휘재
+     */
+    ArtistDto ArtistSongLikeCntNow(int artistId);
+
+    /**
+     * 가수 좋아요 조회
+     *
+     * @author 임휘재
+     */
+    int ArtistLikeCnt(int artistId);
+
+    /**
+     * 가수 좋아요를 좋아요 테이블에 저장
+     *
+     * @author 임휘재
+     */
+    void artistLikeToUserLike(int artistId, String memberId);
 
 
 
