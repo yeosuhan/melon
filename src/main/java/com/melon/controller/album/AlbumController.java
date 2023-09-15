43acc01b488d9f21458f9580d69cc25c7be934e5
dlagnlwo <105468233+dlagnlwo@users.noreply.github.com>
2023-09-15 14:30:36 +0900

    Add files via upload

--- src/main/java/com/melon/controller/album/AlbumController.java
+++ src/main/java/com/melon/controller/album/AlbumController.java
@@ -2,12 +2,13 @@ package com.melon.controller.album;
 
 import java.util.List;
 
+import com.melon.dto.album.AlbumDetails;
+import com.melon.dto.comment.CommentDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import com.melon.dto.album.AlbumDto;
 import com.melon.dto.playlistnow.PlaylistnowDto;
@@ -25,15 +26,88 @@ public class AlbumController {
 	
 	private final IAlbumService albumService;
 	private final IPlaylistnowService playlistnowService;
-	
-    /**
-     * 앨범 수록곡 조회, 앨범상세 조회, 댓글 작성 및 조회
-     * @author 임휘재
-     */
-    @GetMapping
-    public String album_song(){
-        return "album/album";
-    }
+
+	/**
+	 * 앨범 정보, 수록곡, 상세, 댓글 조회
+	 * @author 임휘재
+	 */
+	@GetMapping("/{albumId}")
+	public String albumDetails(@PathVariable("albumId") int albumId,
+							   Model model){
+		AlbumDetails albumDetails = albumService.getAlbumDetails(albumId);
+		model.addAttribute("albumDetails", albumDetails);
+
+		List<AlbumDetails> getAlbumSongList = albumService.getAlbumSongList(albumId);
+		model.addAttribute("getAlbumSongList", getAlbumSongList);
+
+		AlbumDetails albumDetail = albumService.getAlbumDetail(albumId);
+		model.addAttribute("albumDetail", albumDetail);
+
+		List<CommentDto> albumCommentList = albumService.getAlbumCommentList(albumId);
+		log.info("Number of comments retrieved: {}", albumCommentList.size());
+		model.addAttribute("albumComments", albumCommentList);
+		if (!albumCommentList.isEmpty()) {
+			log.info("memberId : {}", albumCommentList.get(0).getMemberId());
+			log.info("albumId : {}", albumCommentList.get(0).getAlbumId());
+		} else {
+			log.info("No albums found for this artist.");
+		}
+		return "album/album";
+	}
+
+
+	/**
+	 * 앨범 댓글 작성
+	 * @author 임휘재
+	 */
+	@PostMapping("/{albumId}/comment/write")
+	public String addUserComment(@ModelAttribute CommentDto commentDto,
+								 @PathVariable("albumId") int albumId) {
+		log.info("dto : {}", commentDto);
+		albumService.saveComment(commentDto, albumId);
+		return "redirect:/album/" + albumId;
+	}
+
+	/**
+	 * 앨범 댓글 삭제
+	 * @author 임휘재
+	 */
+	@PostMapping("/{albumId}/comment/delete/{commentId}")
+	public String deleteUserComment(@PathVariable("albumId") int albumId,
+									@PathVariable("commentId") int commentId) {
+		albumService.deleteComment(commentId, albumId);
+		return "redirect:/album/" + albumId;
+	}
+
+	@PostMapping("/{albumId}/like/update")
+	public ResponseEntity<Integer> albumLikeCntUpdate(@PathVariable("albumId") int albumId) {
+		try {
+			String memberId = "admin";
+			albumService.albumLikeUpdate(albumId);
+			AlbumDetails dto = albumService.selectAlbumLike(albumId);
+			log.info("selectAlbumLike : {}", dto.getAlbumLike());
+			albumService.albumLikeToUserLike(albumId, memberId);
+			log.info("앨범 좋아요 증가를 user_like 테이블에 저장. memberId: {}", memberId);
+			return ResponseEntity.ok(dto.getAlbumLike());
+		} catch (Exception e) {
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(-1);
+		}
+	}
+
+	@PostMapping("/{albumId}/like/delete")
+	public ResponseEntity<Integer> albumLikeCntDelete(@PathVariable("albumId") int albumId) {
+		try {
+			String memberId = "admin";
+			albumService.albumLikeDelete(albumId);
+			AlbumDetails dto = albumService.selectAlbumLike(albumId);
+			log.info("selectAlbumLike : {}", dto.getAlbumLike());
+			albumService.albumLikeToUserLike(albumId, memberId);
+			log.info("앨범 좋아요 감소를 user_like 테이블에 저장. memberId: {}", memberId);
+			return ResponseEntity.ok(dto.getAlbumLike());
+		} catch (Exception e) {
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(1);
+		}
+	}
     
 	/**
 	* 최신앨범 조회
