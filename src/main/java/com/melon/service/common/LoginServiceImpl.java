4e7316c180016545b28321992a7049b9ba581748
BANG <82694273+Bangsuhyun96@users.noreply.github.com>
2023-09-15 14:41:21 +0900

    230915 병합

--- src/main/java/com/melon/service/common/LoginServiceImpl.java
+++ src/main/java/com/melon/service/common/LoginServiceImpl.java
@@ -1,8 +1,11 @@
 package com.melon.service.common;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Service;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -11,29 +14,40 @@ import com.melon.dao.common.ILoginDao;
 
 @Service
 @RequiredArgsConstructor
+@Log4j2
 public class LoginServiceImpl implements ILoginService {
 
 	private final ILoginDao ILoginDao;
 	
 	@Override
-	public String login(LoginDto m, HttpSession session, RedirectAttributes rttr) {
+	public String login(LoginDto m, HttpSession session, RedirectAttributes rttr, HttpServletResponse response) {
 		LoginDto loginDto = ILoginDao.login(m);
 		if(loginDto != null) {
+
 			session.setAttribute("user", loginDto);
-			rttr.addFlashAttribute("msgTitle", "Success !");
 			session.setMaxInactiveInterval(60*100);
+
+			Cookie cookie = new Cookie("usercookie", loginDto.getId());
+			cookie.setMaxAge(60*60*24);
+			response.addCookie(cookie);
+
 			rttr.addFlashAttribute("msg", "로그인 성공");
 			return "redirect:/";
 		}else {
-			rttr.addFlashAttribute("msgTitle", "Failed !");
 			rttr.addFlashAttribute("msg", "로그인 실패");
 			return "redirect:/login/melon";
 		}
 	}
 
 	@Override
-	public void logout(LoginDto m, HttpSession session, RedirectAttributes rttr) {
+	public void logout(LoginDto m, HttpSession session, RedirectAttributes rttr, HttpServletResponse response) {
+
 		session.invalidate();
+
+		Cookie cookie = new Cookie("usercookie", null);
+		cookie.setMaxAge(0);
+		response.addCookie(cookie);
+
 		rttr.addFlashAttribute("msgTitle","Success !");
 		rttr.addFlashAttribute("msg", "로그아웃 성공");
 	}
