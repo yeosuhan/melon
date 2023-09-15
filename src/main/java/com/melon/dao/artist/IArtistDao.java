3b746c264e2b6acf12a632d3a34dbe9a5e4d298c
dlagnlwo <105468233+dlagnlwo@users.noreply.github.com>
2023-09-15 14:41:46 +0900

    Add files via upload

--- src/main/java/com/melon/dao/artist/IArtistDao.java
+++ src/main/java/com/melon/dao/artist/IArtistDao.java
@@ -5,7 +5,6 @@ import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
-import java.util.Map;
 
 @Mapper
 public interface IArtistDao {
@@ -18,7 +17,7 @@ public interface IArtistDao {
     ArtistDto selectArtistInfo(@Param("artistId") int artistId);
 
     /**
-     * 가수 노래
+     * 가수 노래 최신순
      *
      * @author 임휘재
      */
@@ -38,6 +37,83 @@ public interface IArtistDao {
      */
     ArtistDto selectArtistActivity(@Param("artistId") int artistId);
 
+    /**
+     * 가수 노래 인기순(좋아요 기준)
+     *
+     * @author 임휘재
+     */
+    List<ArtistDto> ArtistPopularSongsSort(@Param("artistId") int artistId);
+
+    /**
+     * 가수 노래 가나다순
+     *
+     * @author 임휘재
+     */
+    List<ArtistDto> ArtistSongNameSort(@Param("artistId") int artistId);
+
+    /**
+     * 가수 앨범 목록 중복 제거
+     *
+     * @author 임휘재
+     */
+    List<ArtistDto> ArtistAlbumsRemoveDuplication(@Param("artistId") int artistId);
+
+    /**
+     * 가수 좋아요 증가
+     *
+     * @author 임휘재
+     */
+    void ArtistLikeUpdate(@Param("artistId") int artistId);
+
+    /**
+     * 가수 좋아요 삭제
+     *
+     * @author 임휘재
+     */
+    void ArtistLikeDelete(@Param("artistId") int artistId);
+
+    /**
+     * 현재 가수 좋아요 수
+     *
+     * @author 임휘재
+     */
+    ArtistDto ArtistLikeCntNow(@Param("artistId") int artistId);
+
+    /**
+     * 가수 곡 좋아요 증가
+     *
+     * @author 임휘재
+     */
+    void ArtistSongLikeUpdate(@Param("artistId") int artistId);
+
+    /**
+     * 가수 곡 좋아요 삭제
+     *
+     * @author 임휘재
+     */
+    void ArtistSongLikeDelete(@Param("artistId") int artistId);
+
+    /**
+     * 현재 가수 곡 좋아요 수
+     *
+     * @author 임휘재
+     */
+    ArtistDto ArtistSongLikeCntNow(@Param("artistId") int artistId);
+
+
+    /**
+     * 가수 좋아요 조회
+     *
+     * @author 임휘재
+     */
+    int ArtistLikeCnt(@Param("artistId") int artistId);
+
+    /**
+     * 가수 좋아요를 좋아요 테이블에 저장
+     *
+     * @author 임휘재
+     */
+    void ArtistLikeToUserLike(@Param("artistId") int artistId, @Param("memberId") String memberId);
 
 
 }
