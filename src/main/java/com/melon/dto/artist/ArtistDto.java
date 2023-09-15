c6a5ce0c2bef1803b7d795fcf2afec484ad88d40
dlagnlwo <105468233+dlagnlwo@users.noreply.github.com>
2023-09-15 15:32:28 +0900

    Add files via upload

--- src/main/java/com/melon/dto/artist/ArtistDto.java
+++ src/main/java/com/melon/dto/artist/ArtistDto.java
@@ -1,5 +1,26 @@
 package com.melon.dto.artist;
 
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
 public class ArtistDto {
+    private int albumId;
+    private int artistId;
+    private int songId;
+    private String artistName;
+    private String debut;
+    private String agency;
+    private String activityType;
+    private int artistLike;
+    private byte[] artistFile;
+    private String songName;
+    private String albumName;
+    private String releaseDate;
+    private String genreName;
+    private String artistPro;
+    private int albumLike;
+    private int songLike;
 
 }
