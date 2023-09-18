3ebe38989d28a1208a029770e20fefd9485013b2
dlagnlwo <105468233+dlagnlwo@users.noreply.github.com>
2023-09-18 10:53:24 +0900

    Update ISongDao.java

--- src/main/java/com/melon/dao/song/ISongDao.java
+++ src/main/java/com/melon/dao/song/ISongDao.java
@@ -97,4 +97,31 @@ public interface ISongDao {
 	 * @return 
 	*/
 	public List<Integer> selectSongIds(String SongName);
+	/**
+	 * 노래 좋아요 증가
+	 * @author 임휘재
+	 * @return
+	 */
+	void songLikeAdd(@Param("songId") int songId);
+
+	/**
+	 * 노래 좋아요 감소
+	 * @author 임휘재
+	 * @return
+	 */
+	void songLikeDel(@Param("songId") int songId);
+
+	/**
+	 * 노래 좋아요 조회
+	 * @author 임휘재
+	 * @return
+	 */
+	SongLike songLikeSelect(@Param("songId") int songId);
+
+	/**
+	 * 노래 좋아요를 좋아요 테이블에 저장
+	 * @author 임휘재
+	 * @return
+	 */
+	void songLikeToUserLike(@Param("songId") int songId, @Param("memberId") String memberId);
 }
